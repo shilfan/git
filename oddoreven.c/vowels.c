3cbb80474ef31bd9d4785c1ee9b3b11db1cfe85e
shilfan <shilfan.ahamed29@gmail.com>
2017-06-10 10:08:33 +0530

    Create vowels.c

---
+++ oddoreven.c/vowels.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char ab;
+scanf("%c",ab);
+if(ab =='a' || ab=='e'|| ab=='i'|| ab=='o'||ab=='u' ) 
+{
+	printf("vowels");
+}
+else 
+{
+	printf("consonant");
+	
+}
+return 0;
+}
